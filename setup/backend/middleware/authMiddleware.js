e1e1e7822ea1cef7f83b72a517c3165f1055b30d
Ali Nategh <alinategh005@gmail.com>
2025-10-31 15:55:31 -0400

    Update: improve jwt function to contain user info

--- setup/backend/middleware/authMiddleware.js
+++ setup/backend/middleware/authMiddleware.js
@@ -1,17 +1,17 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-export const protect = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1]; // "Bearer <token>"
+export const protect = async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) {
-    return res.status(401).json({ message: "Not authorized, no token" });
-  }
+    if (!token)
+        return res.status(401).json({message: "Not authorized, no token"});
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // attach user info
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select("-password");
+        next();
+    } catch (error) {
+        res.status(401).json({message: "Invalid token"});
+    }
 };
