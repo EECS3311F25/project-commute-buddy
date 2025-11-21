011dd891575d36e1e0f6ac82309259c6fb5a7e0e
Shaun Fernando <shaunerash@gmail.com>
2025-11-20 20:47:39 -0500

    Updated Routes to include function calls

--- setup/backend/routes/messageRoutes.js
+++ setup/backend/routes/messageRoutes.js
@@ -10,15 +10,15 @@ import {
 const router = Router();
 
 //create or get a chat room with a specific user
-router.post("/open-or-create/:friendID", protect, verifyToken);
+router.post("/open-or-create/:friendID", protect, verifyToken, openOrCreateChat);
 
 //get all chatroom for a loggedin user
-router.get("/my-chats", protect, verifyToken);
+router.get("/my-chats", protect, verifyToken, getMyChats);
 
 //get all messages for a specific chatRoom
-router.get("/:chatRoomId/messages", protect, verifyToken);
+router.get("/:chatRoomId/messages", protect, verifyToken, getMessagesByChatId);
 
 //send a message in a chat
-router.post("/:chatRoomId/send", protect, verifyToken);
+router.post("/:chatRoomId/send", protect, verifyToken, sendMessage);
 
 export default router;
