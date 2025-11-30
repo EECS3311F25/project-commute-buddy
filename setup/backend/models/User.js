165b75d4b7aac789da8b4652f0e55881a216be95
MohammedAshrafulIslam <ashraf22@my.yorku.ca>
2025-11-30 18:21:26 -0500

    feat: add faculty, program, availability to user model

--- setup/backend/models/User.js
+++ setup/backend/models/User.js
@@ -42,6 +42,21 @@ const userSchema = new mongoose.Schema({
     ],
     default: null,
   },
+  faculty: { type: String, default: null },
+  program: { type: String, default: null },
+  availabilityWindow: {
+    type: String,
+    enum: [
+      "Early Morning (5-7 AM)",
+      "Morning (7-9 AM)",
+      "Midday (9 AM-1 PM)",
+      "Afternoon (1-4 PM)",
+      "Evening (4-7 PM)",
+      "Late Evening (7-10 PM)",
+      null,
+    ],
+    default: null,
+  },
 },
   {timestamps: true}
 );
